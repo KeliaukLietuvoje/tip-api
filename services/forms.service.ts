bb5b4bd291657b95fac216cbc16c401ad9752340
LWangllix <vanglikas.lukas@gmail.com>
2024-10-15 01:55:47 +0300

    fix

--- services/forms.service.ts
+++ services/forms.service.ts
@@ -1080,7 +1080,7 @@ export default class FormsService extends moleculer.Service {
   async 'forms.updated'(ctx: Context<EntityChangedParams<Form>>) {
     const { oldData: prevForm, data: form } = ctx.params;
 
-    if (prevForm?.status == FormStatus.APPROVED) {
+    if (form?.status == FormStatus.APPROVED) {
       await this.createFormHistory(ctx, form.id, FormHistoryTypes.UPDATED);
       await this.refreshObjects(ctx);
       return 
