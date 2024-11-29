4d6379d125e41d001c701f4883a95d8ad53b14b8
LWangllix <vanglikas.lukas@gmail.com>
2024-11-29 02:42:43 +0200

    test

--- services/forms.service.ts
+++ services/forms.service.ts
@@ -468,9 +468,12 @@ export default class FormsService extends moleculer.Service {
   async deleteForms(ctx: Context<{ ids: number[] }>) {
     const { ids } = ctx.params;
 
+    const forms: Form[] = await ctx.call('forms.resolve', {
+      id: ids,
+    });
+
     return ctx.call('forms.removeMany', {
-      query: { id: { $in: ids } },
-      scope: AUTH_PROTECTED_SCOPES,
+      query: { id: { $in: forms.map((form) => form.id) } },
     });
   }
 
